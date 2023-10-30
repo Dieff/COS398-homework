92a34528230ecc3e9fec0906fdcbe9ca161c49b6
Nick Dieff <nick.dieff@gmail.com>
2023-10-29 20:50:33 -0400

    hw2 created full layout

--- hw3/readme.txt
+++ hw3/readme.txt
@@ -1,2 +1,4 @@
 This site can be viewed at: https://dieff.github.io/COS398-homework/hw3/
 
+Sources
+- https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_animations/Using_CSS_animations
---
+++ hw3/scripts/mobile-menu.js
@@ -0,0 +1,29 @@
+function whenHeaderVisible(mobileMenu) {
+  mobileMenu.style.display = "none";
+}
+
+function whenHeaderHidden(mobileMenu) {
+  console.log(mobileMenu);
+  mobileMenu.style.display = "flex";
+}
+
+function setupMobileMenu() {
+  const header = document.querySelector("header");
+  const mobileMenu = document.querySelector(".mobile-nav");
+  // the IntersectionObserver tells if an element is visible.
+  // I am using it to tell if the header is or is not visible
+  // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+  const obs = new IntersectionObserver(
+    ([event], _obs) => {
+      if (event.isIntersecting) {
+        whenHeaderVisible(mobileMenu);
+      } else {
+        whenHeaderHidden(mobileMenu);
+      }
+    },
+    { threshold: 0.15 }
+  );
+  obs.observe(header);
+}
+
+window.addEventListener("load", setupMobileMenu);
